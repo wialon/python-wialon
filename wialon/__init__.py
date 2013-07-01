0edba6896617c821957a883c1772a5478ab5b89e
chal Alex Chernetsky <chal@gurtam.com>
2013-07-01 14:53:57 +0300

    Update version to 1.0.1

--- wialon/__init__.py
+++ wialon/__init__.py
@@ -12,6 +12,6 @@ __author__ = "Alex Chernetsky chal@gurtam.com"
 __copyright__ = ("Copyright 2013, Gurtam; ",)
 
 __credits__ = ["Alex Chernetsky", ]
-__version__ = "1.0"
+__version__ = "1.0.1"
 
 __all__ = ["Wialon"]
