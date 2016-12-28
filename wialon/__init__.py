b00c6152fdbf85e91bfd427962ce29aa72374e1b
Aleksey Shmigelski <shmi@gurtam.com>
2016-12-28 10:42:48 +0300

    Fix ImportError during pip install. Fix #6

--- wialon/__init__.py
+++ wialon/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import flags
+from . import flags
 
 from .api import Wialon, WialonError
 # Silence potential warnings from static analysis tools:
@@ -12,6 +12,6 @@ __author__ = "Alex Chernetsky chal@gurtam.com"
 __copyright__ = ("Copyright 2013-2016, Gurtam; ",)
 
 __credits__ = ["Alex Chernetsky", "Aleksey Shmigelski"]
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 __all__ = ["Wialon"]
